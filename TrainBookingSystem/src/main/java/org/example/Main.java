fad73ed66ff84669f39c8765e5965ab5cea991dc
LAPTOP-ORAVP5FB\HUNG <duchungkimchung@gmail.com>
2023-09-19 00:21:40 +0700

    fake lews

--- TrainBookingSystem/src/main/java/org/example/Main.java
+++ TrainBookingSystem/src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Huy Tam Project");
-        System.out.println("Huy Tam Respone");
+        System.out.println("Huy Tam gay lo  Respone");
     }
 }
