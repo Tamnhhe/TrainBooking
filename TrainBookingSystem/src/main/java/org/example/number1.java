3293700fdb77bec9e2fa9b8d8539b5c2fe9a929e
LAPTOP-ORAVP5FB\HUNG <duchungkimchung@gmail.com>
2023-09-19 00:13:23 +0700

    update 12 : 13

---
+++ TrainBookingSystem/src/main/java/org/example/number1.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Huy Tam Project");
+        System.out.println("Huy Tam Respone");
+    }
+}
