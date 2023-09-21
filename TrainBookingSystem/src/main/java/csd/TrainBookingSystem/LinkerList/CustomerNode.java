67b291b05fb3d5cfa300b4feac8e763bad53596a
LAPTOP-ORAVP5FB\HUNG <duchungkimchung@gmail.com>
2023-09-21 18:31:42 +0700

    new

--- TrainBookingSystem/src/main/java/csd/TrainBookingSystem/LinkerList/CustomerNode.java
+++ TrainBookingSystem/src/main/java/csd/TrainBookingSystem/LinkerList/CustomerNode.java
@@ -3,10 +3,26 @@
  */
 package csd.TrainBookingSystem.LinkerList;
 
+import csd.TrainBookingSystem.Entity.Customer;
+import org.w3c.dom.Node;
+
 /**
  * @author QUANG TRUNG
  *
  */
 public class CustomerNode {
+
+Customer info;
+
+Node next;
+
+CustomerNode() {};
+
+CustomerNode(Customer x;Node q){info = x;next=q};
+
+CustomerNode(Customer x){this(x,null);}
+
+
+
 
 }
