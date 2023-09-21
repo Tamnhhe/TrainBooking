4e58ce97229892a8b4a01da74bc7e390286bfce0
LAPTOP-ORAVP5FB\HUNG <duchungkimchung@gmail.com>
2023-09-21 20:54:31 +0700

    update booking

--- TrainBookingSystem/src/main/java/csd/TrainBookingSystem/Entity/Booking.java
+++ TrainBookingSystem/src/main/java/csd/TrainBookingSystem/Entity/Booking.java
@@ -8,5 +8,40 @@ package csd.TrainBookingSystem.Entity;
  *
  */
 public class Booking {
+    String tcode;
+    String ccode;
+    int seat;
 
+    public Booking() {
+    }
+
+    public Booking(String tcode, String ccode,int seat) {
+        this.ccode = ccode;
+        this.tcode=tcode;
+        this.seat = seat;
+    }
+
+    public String getCcode() {
+        return ccode;
+    }
+
+    public void setCcode(String ccode) {
+        this.ccode = ccode;
+    }
+
+    public int getSeat() {
+        return seat;
+    }
+
+    public void setSeat(int seat) {
+        this.seat = seat;
+    }
+
+    public String getTcode() {
+        return tcode;
+    }
+
+    public void setTcode(String tcode) {
+        this.tcode = tcode;
+    }
 }
