1dc7af6cd739701a8113e08795d2627c52035f63
MSI\QUANG TRUNG <if.phoenix93@gmail.com>
2023-09-21 18:06:47 +0700

    update app

--- TrainBookingSystem/src/main/java/csd/TrainBookingSystem/App.java
+++ TrainBookingSystem/src/main/java/csd/TrainBookingSystem/App.java
@@ -1,5 +1,11 @@
 package csd.TrainBookingSystem;
 
+import csd.TrainBookingSystem.LinkerList.BookingNode;
+import csd.TrainBookingSystem.LinkerList.CustomerNode;
+import csd.TrainBookingSystem.LinkerList.TrainNode;
+
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -7,8 +13,33 @@ package csd.TrainBookingSystem;
 
 public class App 
 {
+        Scanner sc = new Scanner(System.in);
     public static void main( String[] args )
     {
-        
+        TrainNode trainHead = null;
+        CustomerNode customerHead = null;
+        BookingNode bookingHead = null;
+
+
+
+
     }
+
+    public void menu(){
+        boolean menucheck=true;
+        do {
+            System.out.println("TRAIN BOOKING SYSTEM\n" +
+                    "1. Train list\n" +
+                    "2. Customer list\n" +
+                    "3. Booking list");
+            int choice = Integer.parseInt(sc.nextLine());
+
+            switch (choice){
+                case 1: 
+            }
+
+
+        }while(menucheck);
+    }
+
 }
